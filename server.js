227fa23ad1cd77ab59a5826976b6feb7b842bd59
jaime c fajardo <juncfaj@gmail.com>
2019-02-25 20:11:31 -0500

    fix css

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-
+var path = require('path');
 var port = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the 'public' directory
-app.use(express.static(process.cwd() + '/public'));
+console.log(path.join(__dirname , 'public'));
+app.use(express.static(path.join(__dirname , 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -22,8 +23,10 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them
-var routes = require('./controllers/burger_controller.js');
+var routes = require('./controllers/burger_controller');
 
+
+//'./controllers/burger_controller.js'
 app.use('/', routes);
 
 app.listen(port);
